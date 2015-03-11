e98850811684a85b816cf9e6bb925b64f6d2f0ee
Keith Watanabe <keithwatanabe@Keiths-MacBook-Pro.local>
2015-03-11 15:07:56 -0700

    added comment

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	protected $table = 'users';
 
 	/**
-	 * The attributes that are mass assignable.
+	 * The attributes that are mass assignable. Will expand as needed
 	 *
 	 * @var array
 	 */
